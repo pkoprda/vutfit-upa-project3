c5cf60185f94c204fbb255ce587e9aac885922c3
pkoprda <petokoprda@gmail.com>
2022-11-26 01:06:56 +0100

    Implemented getting urls from website

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# vutfit-upa-project3
-Data extraction from the web
+# UPA projekt, 3. časť: Extrakcia dát z webu
+
+**Názov tímu:** FIT gigachads 🍆
+
+**Autori**:
+
+- Bc. Ivan Halomi <xhalom00@stud.fit.vutbr.cz>
+- Bc. Peter Koprda <xkoprd00@stud.fit.vutbr.cz>
+- Bc. Adam Múdry <xmudry01@stud.fit.vutbr.cz>
+
+</br>
+
+**Názov e-shopu**: Dell Technologies
+
+**URL**: https://www.dell.com/en-us/shop/gaming-gaming-accessories/ar/6488
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_content_website(page: int, data_dir='data'):
+    if not os.path.exists(data_dir):
+        try:
+            os.mkdir(data_dir)
+        except OSError as error:
+            print(error)
+
+    request = requests.get(f"https://www.dell.com/en-us/shop/gaming-gaming-accessories/ar/6488?page={page}")
+    soup = BeautifulSoup(request.content, 'html.parser')
+    with open(f"{data_dir}/website-page-{page}.html", 'w') as html_file:
+        html_file.write(soup.prettify())
+
+
+def get_urls(data_dir='data'):
+    if os.path.exists('urls.txt'):
+        os.remove('urls.txt')
+
+    for page in range(1, 26):
+        html_file = f"{data_dir}/website-page-{page}.html"
+        if not os.path.exists(html_file):
+            get_content_website(page)
+
+        with open(html_file, 'r') as f:
+            html_doc = f.read().rstrip()
+        soup = BeautifulSoup(html_doc, 'html.parser')
+
+        for section in soup.body.main.find_all('section', attrs={"class": "ps-top"}):
+            for link in section.find_all('div', attrs={"class": "ps-image ps-snp-product-image"}):
+                with open('urls.txt', 'a') as urls_file:
+                    urls_file.write(f"https:{link.find('a').get('href')}\n")
+
+
+if __name__ == "__main__":
+    get_urls()
