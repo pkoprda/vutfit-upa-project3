5a77bee0e5c34406297a3dd55322e45e53f71044
Sevin556 <ivan.halomi@gmail.com>
2022-12-01 15:05:39 +0100

    Added function for reading price and name of products

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
+
 def get_content_website(page: int, data_dir='data'):
     if not os.path.exists(data_dir):
         try:
@@ -34,6 +35,34 @@ def get_urls(data_dir='data'):
                 with open('urls.txt', 'a') as urls_file:
                     urls_file.write(f"https:{link.find('a').get('href')}\n")
 
+def get_product_page(product_url):
+    headers = {'content-type': 'application/json', 'user_agent': 'Mozilla/5.0'}
+    product_url = product_url[:-1]
+    request = requests.get(product_url, headers=headers)
+    soup = BeautifulSoup(request.content, 'html.parser')
+    
+    product_name = soup.body.find('h1').span.contents[0].strip()
+    product_price = soup.body.find('div', attrs={'class': "ps-dell-price ps-simplified"})
+    try:
+        product_price = product_price.find_all('span')[1].contents[0].strip()
+    except:
+        product_price = product_price.contents[0].strip()
+
+    with open('data.tsv', 'a') as data_file:
+            data_file.write(f"{product_url}\t{product_name}\t{product_price}\n")
+    
+
+def get_name_price():
+    if os.path.exists('data.tsv'):
+        os.remove('data.tsv')
+    if not os.path.exists('urls.txt'):
+        get_urls()
+
+    with open("urls.txt", 'r') as f:
+        lines = f.readlines()
+        for product_url in lines:
+            get_product_page(product_url)
 
 if __name__ == "__main__":
     get_urls()
+    get_name_price()
